6eb0fc2c02bc3e4951d1120f7e675992b2e216f3
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-03-15 13:03:00 +0100

    AddViewComponents,AddPartial

--- DataAccessLayer/Repository/GenericRepository.cs
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -3,6 +3,7 @@ using DataAccessLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,7 +20,8 @@ namespace DataAccessLayer.Repository
 
         public T GetByID(int id)
         {
-            throw new NotImplementedException();
+            using var c = new Context();
+            return c.Set<T>().Find(id);
         }
 
         public List<T> GetList()
@@ -28,16 +30,24 @@ namespace DataAccessLayer.Repository
             return c.Set<T>().ToList();
         }
 
+        public List<T> GetListByFilter(Expression<Func<T, bool>> filter)
+        {
+            using var c = new Context();
+            return c.Set<T>().Where(filter).ToList();
+        }
+
         public void Insert(T t)
         {
             using var c = new Context();
             c.Add(t);
+            c.SaveChanges();
         }
 
         public void Update(T t)
         {
             using var c = new Context();
             c.Update(t);
+            c.SaveChanges();
         }
     }
 }
