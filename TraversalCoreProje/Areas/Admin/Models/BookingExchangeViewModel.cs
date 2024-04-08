348bcb32919bba1c13dcd9a617aaac5c40739384
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-04-09 01:56:55 +0200

    Booking Api and Imdb Top 100 List Api

---
+++ TraversalCoreProje/Areas/Admin/Models/BookingExchangeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TraversalCoreProje.Areas.Admin.Models
+{
+    public class BookingExchangeViewModel
+    {
+        public  string base_currency_date { get; set; }
+        public  string base_currency { get; set; }
+    }
+}
