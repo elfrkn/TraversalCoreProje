666bea9a2614814e7d66e0203654e8d51e61c2dc
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-05-16 16:48:02 +0200

    .

--- TraversalCoreProje/Areas/Member/Controllers/DestinationController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/DestinationController.cs
@@ -7,6 +7,7 @@ namespace TraversalCoreProje.Areas.Member.Controllers
 {
     [AllowAnonymous]
     [Area("Member")]
+    [Route("Member/[controller]/[action]")]
     public class DestinationController : Controller
     {
         DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
@@ -14,6 +15,18 @@ namespace TraversalCoreProje.Areas.Member.Controllers
         {
             var values = destinationManager.TGetList();
             return View(values);
+        }
+
+        public IActionResult GetCitiesSearchByName(string searchString)
+        {
+            ViewData["CurrentFilter"] = searchString;
+            var values = from x in destinationManager.TGetList() select x;
+
+            if (!string.IsNullOrEmpty(searchString))
+                {
+                values = values.Where(y => y.City.Contains(searchString));
+            }
+            return View(values.ToString());
         }
     }
 }
